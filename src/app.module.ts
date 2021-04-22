b63f1914b17ea8a7d62ab9ce19d6e8586f88e739
SuelenGC <suelengcarvalho@gmail.com>
2021-04-22 02:05:19 -0300

    Add config .env file and dependecy

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,24 @@ import { AppService } from './app.service';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { LivrosModule } from './livros/livros.module';
+import { ConfigModule } from '@nestjs/config/dist/config.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-      autoLoadEntities: true,
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      logging: false,
+      entities: [
+        "dist/**/*.entity{.ts,.js}"
+      ],
+      synchronize: true,
+      autoLoadEntities: true
     }), LivrosModule,
   ],
   controllers: [AppController],
